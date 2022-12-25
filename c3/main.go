ffc93edd6d21d72b67a91493d72a5911799670f4
kokoakuma <aikw9101@gmail.com>
2022-12-25 13:30:41 +0000

    add 3-13

--- c3/main.go
+++ c3/main.go
@@ -3,27 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/cookiejar"
 	"net/http/httputil"
 )
 
 func main() {
-	jar, err := cookiejar.New(nil)
+	transport := &http.Transport{}
+	transport.RegisterProtocol("file", http.NewFileTransport(http.Dir(".")))
+	client := http.Client{
+		Transport: transport,
+	}
+
+	resp, err := client.Get("file://./main.go")
 	if err != nil {
 		panic(err)
 	}
-	client := http.Client{
-		Jar: jar,
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
 	}
-	for i := 0; i < 2; i++ {
-		resp, err := client.Get("http://localhost:18888/cookie")
-		if err != nil {
-			panic(err)
-		}
-		dump, err := httputil.DumpResponse(resp, true)
-		if err != nil {
-			panic(err)
-		}
-		log.Println(string(dump))
-	}
+	log.Println(string(dump))
+
 }
