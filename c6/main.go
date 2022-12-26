194c32ff9a135cd9eff285607f171d3f96596387
kokoakuma <aikw9101@gmail.com>
2022-12-26 14:28:22 +0000

    use socket and chunk data

--- c6/main.go
+++ c6/main.go
@@ -1,34 +1,80 @@
 package main
 
 import (
-	"crypto/tls"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
-	"net/http/httputil"
+	"time"
 )
 
 func main() {
-	cert, err := tls.LoadX509KeyPair("../client.crt", "../client.key")
+
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	conn, err := dialer.Dial("tcp", "localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+
+	request, _ := http.NewRequest("GET", "http://localhost:18888/upgrade", nil)
+	request.Header.Set("Connection", "Upgrade")
+	request.Header.Set("Upgrade", "MyProtocol")
+	err = request.Write(conn)
 	if err != nil {
 		panic(err)
 	}
 
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				Certificates: []tls.Certificate{cert},
-			},
-		},
-	}
-	resp, err := client.Get("https://localhost:18443")
+	resp, err := http.ReadResponse(reader, request)
 	if err != nil {
 		panic(err)
 	}
 
-	defer resp.Body.Close()
-	dump, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		panic(err)
+	log.Println("Status:", resp.Status)
+	log.Println("Headers:", resp.Header)
+
+	counter := 10
+	for {
+		data, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Println("receive <-", string(bytes.TrimSpace(data)))
+		fmt.Fprintf(conn, "%d\n", counter)
+		fmt.Println("send ->", counter)
+		counter--
 	}
-	log.Println(string(dump))
 }
+
+// func mainWithCrt() {
+// 	cert, err := tls.LoadX509KeyPair("../client.crt", "../client.key")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	client := &http.Client{
+// 		Transport: &http.Transport{
+// 			TLSClientConfig: &tls.Config{
+// 				Certificates: []tls.Certificate{cert},
+// 			},
+// 		},
+// 	}
+// 	resp, err := client.Get("https://localhost:18443")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	defer resp.Body.Close()
+// 	dump, err := httputil.DumpResponse(resp, true)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	log.Println(string(dump))
+// }
--- server.go
+++ server.go
@@ -1,12 +1,61 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
+
+func handleUpgrade(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("send ->", i)
+		readWriter.Flush()
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("receive <- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func handleChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("Exptected http.ResponseWriter to be an http.Flusher")
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
@@ -20,15 +69,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	server := &http.Server{
-		TLSConfig: &tls.Config{
-			ClientAuth: tls.RequireAndVerifyClientCert,
-			MinVersion: tls.VersionTLS12,
-		},
-		Addr: ":18443",
-	}
+	var httpServer http.Server
 	http.HandleFunc("/", handler)
-	log.Println("start http listening :18443")
-	err := server.ListenAndServeTLS("server.crt", "server.key")
-	log.Println(err)
+	http.HandleFunc("/upgrade", handleUpgrade)
+	http.HandleFunc("/chunked", handleChunkedResponse)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
 }
